696535704501c07197dda260a7ddb513120945fc
Himanshu Bhatta <himanshubhatta666@gmail.com>
2025-04-06 22:34:06 +0545

    final update google search

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://hb-habit-tracker.vercel.app', // replace with your actual domain
+    generateRobotsTxt: true, // (optional)
+    changefreq: 'daily',
+    priority: 0.7,
+  };
+  
