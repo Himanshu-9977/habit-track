b747e436baa76a83e52b6d30497ea9300773abcd
Himanshu Bhatta <himanshubhatta666@gmail.com>
2025-04-06 22:57:14 +0545

    delete head

--- app/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-    return (
-      <>
-        <meta name="google-site-verification" content="dmEhPdWWVlrnQoaQBLhhoQrPMsvXafz1RbnW75a80P0" />
-      </>
-    );
-  }
-  
